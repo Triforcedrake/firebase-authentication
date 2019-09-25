89544ee83a32fb6e7d993362c72fdb3930c1c70a
Triforcedrake <47779609+Triforcedrake@users.noreply.github.com>
2019-09-25 19:56:17 +0200

    Update Login.js

--- scenes/Login.js
+++ scenes/Login.js
@@ -5,12 +5,12 @@ import styles from './Stylesheet/Style';
 import * as firebase from "firebase"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC02Mx0TmUsBaM1R7XCA9mG3Y1YQ6IZcs4",
-  authDomain: "awesomeproject-1548233775133.firebaseapp.com",
-  databaseURL: "https://awesomeproject-1548233775133.firebaseio.com",
-  projectId: "awesomeproject-1548233775133",
-  storageBucket: "awesomeproject-1548233775133.appspot.com",
-  messagingSenderId: "931119102286"
+  apiKey: "---",
+  authDomain: "---",
+  databaseURL: "---",
+  projectId: "---",
+  storageBucket: "---",
+  messagingSenderId: "---"
 };
 
 firebase.initializeApp(firebaseConfig);
